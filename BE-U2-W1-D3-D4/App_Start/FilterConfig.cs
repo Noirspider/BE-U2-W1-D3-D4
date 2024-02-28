8c88f0d73e48d3ad814a44c7054de66e7b578373
EAGLE-3 <EAGLE-3@EAGLE-1>
2024-02-28 17:20:45 +0100

    Aggiungere i file di progetto.

---
+++ BE-U2-W1-D3-D4/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BE_U2_W1_D3_D4
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ BE-U2-W1-D3-D4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BE_U2_W1_D3_D4.MvcApplication" Language="C#" %>
